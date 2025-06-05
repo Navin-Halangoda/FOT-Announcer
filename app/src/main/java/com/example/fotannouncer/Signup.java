308049163be06e471fce0c799b616f4026e6a049
Navin <naveenshanaka186@gmail.com>
2025-06-05 19:23:45 +0530

    create ui of splash,signin,signup,news,devinfo and userinfo

---
+++ app/src/main/java/com/example/fotannouncer/Signup.java
@@ -0,0 +1,32 @@
+package com.example.fotannouncer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Signup extends AppCompatActivity {
+    TextView signinRedirect;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_signup);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    signinRedirect = findViewById(R.id.text_view_signin);
+    signinRedirect.setOnClickListener(v -> {
+        Intent intent = new Intent(Signup.this, MainActivity.class);
+        startActivity(intent);
+    });
+    }
+}
